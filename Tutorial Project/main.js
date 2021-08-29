426e867e5dbe646afb1ae863b6a1da9594772c25
Glagny <nikoicardo@gmail.com>
2021-08-29 00:03:11 -0600

    Created a Three Js Starter Website

--- Tutorial Project/main.js
+++ Tutorial Project/main.js
@@ -1,6 +1,118 @@
-import './style.css'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0x00cc96 });
+const torus = new THREE.Mesh(geometry, material);
+
+
+torus.position.z = -20
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const addStar = (flag) => {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xf0ff4f });
+  flag = !flag;
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+};
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+const myBox = new THREE.TextureLoader().load('me1.jpg');
+
+const me = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3), 
+  new THREE.MeshBasicMaterial( {map: myBox})
+); 
+
+me.position.x = 3; 
+me.position.z = 15; 
+me.position.y = 0; 
+
+scene.add(me);
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32), 
+  new THREE.MeshStandardMaterial({map: moonTexture, normalMap: normalTexture})
+);
+scene.add(moon); 
+
+moon.position.z = -10;
+moon.position.y = 10;
+moon.position.x = -30;
+
+
+const moveCamera = () => {
+
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  me.rotation.y += 0.075;
+  me.rotation.z += 0.05;
+
+  camera.position.z = 20 + t * -.001;
+  camera.position.x = t * -.0002; 
+  camera.rotation.y += 5 + t * -0.2;
+  camera.rotation.z += .01;
+
+}
+
+document.body.onscroll = moveCamera; 
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+};
+
+animate();
